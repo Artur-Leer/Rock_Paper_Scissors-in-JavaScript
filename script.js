27d76bcb4046b4eb6d2098dd325de4e95253f5c3
Artur-Leer <153430292+Artur-Leer@users.noreply.github.com>
2024-02-24 12:39:53 +0100

    Add playGame function

--- script.js
+++ script.js
@@ -1,5 +1,6 @@
+const CHOICES = ["ROCK","PAPER","SCISSORS"];
+
 function getComputerChoice (){
-    const CHOICES = ["ROCK","PAPER","SCISSORS"]
     let index = Math.floor(Math.random() * CHOICES.length);
     return CHOICES[index];
 }
@@ -12,6 +13,32 @@ function getPlayerChoice (){
     let playerChoice = (prompt('ROCK, PAPER OR SCISSORS?').toUpperCase());
     return playerChoice;
 }
+
+function playGame (){
+// DEFINE COMPUTERCHOICE & PLAYERCHOICE
+    const computerChoice = getComputerChoice();
+    const playerChoice = getPlayerChoice();
+// CHECK IF PLAYERCHOICE IS VALID
+    if (validChoice(playerChoice, CHOICES)){
+
+        console.log(`You chose ${playerChoice}`);
+        console.log(`Computer chose ${computerChoice}`);
+
+        if (playerChoice === computerChoice){
+            console.log ('That means tie!');
+        } else if ( playerChoice === "ROCK" && computerChoice === "SCISSORS" ||
+                    playerChoice === "PAPER" && computerChoice === "ROCK" ||
+                    playerChoice === "SCISSORS" && computerChoice === "PAPER"){
+                        console.log('You win!');
+                    } else {
+                        console.log ('Computer wins!');
+                    }
+
+    }}
+    playGame();
+
+    
+
 
 
 
